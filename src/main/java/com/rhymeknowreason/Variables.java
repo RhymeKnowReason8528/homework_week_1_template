e194e591f2da5f3e332d604a8a59bdd7c92ada61
DrSuess2 <truccmom@yahoo.com>
2017-03-18 21:07:56 -0400

    Did variable homework

--- src/main/java/com/rhymeknowreason/Variables.java
+++ src/main/java/com/rhymeknowreason/Variables.java
@@ -5,19 +5,28 @@ public class Variables {
         // Unless told otherwise, make sure all variables conform to standard naming conventions.
 
         // TODO: Make and initialize a variable that stores an integer.
+        int integer1 = 1;
 
         // TODO: Make and don't initialize a variable that stores a decimal number.
+        double double1;
 
         // TODO: Make and initialize a variable that stores either true or false.
+        boolean boolean1 = true;
 
         // TODO: Declare several integer variables on the same line. Exactly two of them should be initialized.
+        int integer2, integer3 = 90, integer4 = 7, integer5, integer6;
 
         // TODO: Make a "constant" decimal variable, that can't change.
+        final double double2 = 0.356;
 
         // TODO: Wipe the value from one of the variables you declared and initialized above.
+        integer1 = 0;
 
         // TODO: Create a variable with a two-word name.
+        byte byteOne = 8;
 
         // TODO: Create a variable with a name that is legal, but that does not follow standard Java convention.
+        boolean Booleantwo = false;
+
     }
 }
