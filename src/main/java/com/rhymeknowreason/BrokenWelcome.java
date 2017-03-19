b43e341ef2d160c1a17a5317c7db6294c0151779
DrSuess2 <truccmom@yahoo.com>
2017-03-18 21:00:16 -0400

    week-1-homework-katiepetrucci

--- src/main/java/com/rhymeknowreason/BrokenWelcome.java
+++ src/main/java/com/rhymeknowreason/BrokenWelcome.java
@@ -3,6 +3,6 @@ package com.rhymeknowreason;
 public class BrokenWelcome {
     public static void main(String args[]) {
         // When you finish with it, this should print "Welcome to the RKR Programming Training!".
-        System.in.println("welcome too the RKR Programmming Training!")
+        System.out.println("Welcome to the RKR Programming Training!");
     }
 }
